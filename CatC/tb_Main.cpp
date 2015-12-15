d644db8909986731ed49e9cfc2b99ca5f9959028
Frédéric Perraud <frederic.perraud@ensimag.grenoble-inp.fr>
2015-12-15 07:53:00 +0100

    dernière version bien

--- CatC/tb_Main.cpp
+++ CatC/tb_Main.cpp
@@ -7,14 +7,15 @@
 using namespace std;
 
 #define WIDTH_IMAGE_T  320
+#define WIDTH_RAM_T  512
 #define HEIGHT_IMAGE_T 240
 #define CELL_WIDTH_T   4
 #define CELL_HEIGHT_T  4
 #define CELL_SIZE_T    16
 
 //simultate input and output memory
-static ac_int<8,false> image[WIDTH_IMAGE_T*HEIGHT_IMAGE_T];
-static ac_int<8,false> hog[WIDTH_IMAGE_T*HEIGHT_IMAGE_T];
+static ac_int<8,false> image[WIDTH_RAM_T*HEIGHT_IMAGE_T];
+static ac_int<8,false> hog[WIDTH_RAM_T*HEIGHT_IMAGE_T];
 
 #include <mc_scverify.h>
 
@@ -25,7 +26,7 @@ CCS_MAIN(unsigned int argc, char *argv[])
   unsigned int j;
 
   //write test file in input memory
-  string file = "empire";
+  string file = "barbara3";
   string magic_number;
   int useless;
   ifstream fichier(("../../img/" + file + ".pgm").c_str(), ios::in);
@@ -36,7 +37,7 @@ CCS_MAIN(unsigned int argc, char *argv[])
  Ram_X : for(j = 0; j < HEIGHT_IMAGE_T; j++) {
   Ram_Y : for(i = 0; i < WIDTH_IMAGE_T; i++) {
       fichier >> useless;
-      image[i+WIDTH_IMAGE_T*j] = useless;
+      image[i+WIDTH_RAM_T*j] = useless;
     }
   }
   fichier.close();
@@ -55,7 +56,7 @@ CCS_MAIN(unsigned int argc, char *argv[])
 	      << "255" << endl;
  Hog_X : for(j = 0; j < HEIGHT_IMAGE_T; j++) {
   Hog_Y : for(i = 0; i < WIDTH_IMAGE_T; i++) {
-      fichier_out << hog[j*WIDTH_IMAGE_T+i] << " ";
+      fichier_out << hog[j*WIDTH_RAM_T+i] << " ";
     }
     fichier_out << endl;
   }
